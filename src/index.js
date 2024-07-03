82e16c210992252dfa70493dfe07180a9674bc20
A.NALLAPERUMAL <139489689+nallaperumal007@users.noreply.github.com>
2024-07-03 11:11:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ class FormBuilder extends Component {
       <div className="bg-gray-100 p-4">
       <div className="bg-white p-4 rounded-md shadow-md">
         <div className="alert alert-info mb-4">
-          This is an example of how to use formBuilder with React. The JSON data for this form was set programmatically.
+          Welecome
         </div>
         <div id="fb-editor" ref={this.fb} />
       </div>
